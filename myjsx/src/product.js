75641b621fdfb67d288118b887ad1f5d8050b6ab
Tharouat <habka.tharouat@gmail.com>
2024-01-21 13:44:26 +0100

    first commit

---
+++ myjsx/src/product.js
@@ -0,0 +1,7 @@
+const product = {
+    name: "Rock & Roll",
+    price: 150.000,
+    description: "Book your ticket for the best rock and roll band",
+    imagePath: "./rock.jpg",
+  };
+  export default product;
